0bace8a436433bb28e7cf2e9077cb1f9df0afd74
sgwon96 <zxcvb5434@likelion.org>
2021-04-14 17:19:51 +0900

    react&JSX

---
+++ week2/lion-app/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="myStyle">안녕하세요</h1>
+      <div className={'post'}>
+        첫 게시글 입니다.
+      </div>
+    </div>
+  );
+}
+
+export default App;
