9c2bb506fe654a8da93a7f31a20fa5e712248a5d
sgwon96 <zxcvb5434@likelion.org>
2021-04-16 17:30:32 +0900

    State&Props

--- week2/lion-app/src/App.js
+++ week2/lion-app/src/App.js
@@ -1,13 +1,62 @@
+import React from 'react';
+import logo from './logo.svg';
 import './App.css';
 
+
+
+
+class WorldClock extends React.Component {
+
+    constructor(props) {
+      super(props)
+      this. state = {
+        hour: this.props.time,
+        minute: 0
+      }
+  
+    setInterval(() => {
+      this.setState((state)=>(
+      state.minute === 59
+      ?{hour: state.hour + 1 , minute: 0}
+      :{minute: state.minute+1}
+      ))
+    }, 100);
+  }
+
+  
+
+  render() {
+    return (
+      <div className={"WorldClock"}> 
+      <h2>도시: {this.props.city}</h2>
+      <p>시간: {this.state.hour}시 {this.state.minute}분</p>
+    </div>
+    )
+  }
+}
+
 function App() {
+  const cityTimeData = [
+    ['서울', 10],
+    ['베이징', 9],
+    ['시드니', 12],
+    ['LA', 17],
+    ['부산', 10]
+  ]
+
+  const WorldClockList = cityTimeData.map((citytime, index)=>
+      <WorldClock city={citytime[0]} time={citytime[1]}/>
+  )
+
   return (
     <div className="App">
       <h1 className="myStyle">안녕하세요</h1>
       <div className={'post'}>
         첫 게시글 입니다.
       </div>
+      {WorldClockList}
     </div>
+
   );
 }
 
